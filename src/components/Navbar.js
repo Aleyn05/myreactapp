1c5a45d354b5dbe72b1382babb3383744670abd9
Aleyn05 <131854150+Aleyn05@users.noreply.github.com>
2024-01-18 18:37:18 +0800

    Update Navbar.js
    Add the menus

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,8 @@ function Navbar() {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <>
             <nav className="navbar">
@@ -15,6 +17,33 @@ function Navbar() {
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                                About
+                            </Link>
+                        </li>
+                         <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                         <li className='nav-item'>
+                            <Link to='/contacts' className='nav-links' onClick={closeMobileMenu}>
+                                Contacts
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/login' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Log In
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
