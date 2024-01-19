48484806ca4ebb03bc99b1fa168106847061167a
Aleyn05 <131854150+Aleyn05@users.noreply.github.com>
2024-01-19 11:58:06 +0800

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Button } from './Button'
 
 function HeroSection() {
   return (
-    <div>
-      
+    <div className='hero-container'>
+        <h1>Where Every Tail Tells a Happy Tale!</h1>
+        <p>Your Pet's Wellness, Our Priority</p>  
+        <div className='hero-btns'>
+            <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+                Start Appointment      
+            </Button>
+        </div>
     </div>
   )
 }
