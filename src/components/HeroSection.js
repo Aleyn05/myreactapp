1d1bed14a146834328893c342789d92809dbd3c3
Aleyn05 <131854150+Aleyn05@users.noreply.github.com>
2024-01-19 21:30:23 +0800

    Update
    Added new codes and images

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import '../App.css';
 import { Button } from './Button'
+import './HeroSection.css'
+
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-        <h1>Where Every Tail Tells a Happy Tale!</h1>
+      <div className='hero-container'>
+        <img src="./hero-banner.png" alt="banner" />
+          <h1>Where Every Tail <br />Tells a Happy Tale!</h1>
         <p>Your Pet's Wellness, Our Priority</p>  
         <div className='hero-btns'>
-            <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+            <Button className='btns'
+                  buttonStyle='btn--outline'
+                  buttonSize='btn--large'>
                 Start Appointment      
+            </Button>
+            <Button className='btns'
+                  buttonStyle='btn--primary'
+                  buttonSize='btn--large'>
+                Hi <i className='far-play-circle'/>     
             </Button>
         </div>
     </div>
